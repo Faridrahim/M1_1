a45508761b6751439f5a70c3b159e5094e883db1
Raqifismayilov <38552428+Raqifismayilov@users.noreply.github.com>
2021-05-17 17:51:58 +0400

    c1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        string s="Hello";
+        String s="Hello";
+        System.out.println(s);
     }
 }
